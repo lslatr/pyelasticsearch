d01cb1cf4b871205a110069e4fa93972019d62bc
Erik Dahl <edahl@erikdahl.org>
2012-05-21 20:26:59 -0400

    move the tests to a package so we can run from setup.py

---
+++ tests/tests.py
@@ -0,0 +1,144 @@
+"""
+Unit tests for pyelasticsearch.  These require an elasticsearch server running on the default port (localhost:9200).
+"""
+import logging
+import unittest
+from pyelasticsearch import ElasticSearch
+
+
+class VerboseElasticSearch(ElasticSearch):
+     def setup_logging(self):
+         log = super(VerboseElasticSearch, self).setup_logging()
+         log.setLevel(logging.DEBUG)
+         return log
+
+
+class ElasticSearchTestCase(unittest.TestCase):
+    def setUp(self):
+        self.conn = ElasticSearch('http://localhost:9200/')
+
+    def tearDown(self):
+        self.conn.delete_index("test-index")
+
+    def assertResultContains(self, result, expected):
+        for (key, value) in expected.items():
+            self.assertEquals(value, result[key])
+
+class IndexingTestCase(ElasticSearchTestCase):
+    def testSetupLogging(self):
+        log = self.conn.setup_logging()
+        self.assertTrue(isinstance(log, logging.Logger))
+        self.assertEqual(log.level, logging.ERROR)
+
+    def testOverriddenSetupLogging(self):
+        conn = VerboseElasticSearch('http://localhost:9200/')
+        log = conn.setup_logging()
+        self.assertTrue(isinstance(log, logging.Logger))
+        self.assertEqual(log.level, logging.DEBUG)
+
+    def testIndexingWithID(self):
+        result = self.conn.index({"name":"Joe Tester"}, "test-index", "test-type", 1)
+        self.assertResultContains(result, {'_type': 'test-type', '_id': '1', 'ok': True, '_index': 'test-index'} )
+
+    def testIndexingWithoutID(self):
+        result = self.conn.index({"name":"Joe Tester"}, "test-index", "test-type")
+        self.assertResultContains(result, {'_type': 'test-type', 'ok': True, '_index': 'test-index'} )
+        # should have an id of some value assigned.
+        self.assertTrue(result.has_key('_id') and result['_id'])
+
+    def testExplicitIndexCreate(self):
+        result = self.conn.create_index("test-index")
+        self.assertResultContains(result, {'acknowledged': True, 'ok': True})
+
+    def testDeleteByID(self):
+        self.conn.index({"name":"Joe Tester"}, "test-index", "test-type", 1)
+        self.conn.refresh(["test-index"])
+        result = self.conn.delete("test-index", "test-type", 1)
+        self.assertResultContains(result, {'_type': 'test-type', '_id': '1', 'ok': True, '_index': 'test-index'})
+
+    def testDeleteByQuery(self):
+        self.conn.index({"name":"Joe Tester"}, "test-index", "test-type", 1)
+        self.conn.index({"name":"Bill Baloney"}, "test-index", "test-type", 2)
+        self.conn.index({"name":"Horace Humdinger"}, "test-index", "test-type", 3)
+        self.conn.refresh(["test-index"])
+
+        self.conn.refresh(["test-index"])
+        result = self.conn.count("*:*", indexes=['test-index'])
+        self.assertResultContains(result, {'count': 3})
+
+        result = self.conn.delete_by_query("test-index", "test-type", {"query_string": {"query": "name:joe OR name:bill"}})
+        self.assertResultContains(result, {'ok': True})
+
+        self.conn.refresh(["test-index"])
+        result = self.conn.count("*:*", indexes=['test-index'])
+        self.assertResultContains(result, {'count': 1})
+
+    def testDeleteIndex(self):
+        self.conn.create_index("another-index")
+        result = self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'acknowledged': True, 'ok': True})
+
+    def testCannotCreateExistingIndex(self):
+        self.conn.create_index("another-index")
+        result = self.conn.create_index("another-index")
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'error': 'IndexAlreadyExistsException[[another-index] Already exists]'})
+
+    def testPutMapping(self):
+        result = self.conn.create_index("test-index")
+        result = self.conn.put_mapping("test-type", {"test-type" : {"properties" : {"name" : {"type" : "string", "store" : "yes"}}}}, indexes=["test-index"])
+        self.assertResultContains(result, {'acknowledged': True, 'ok': True})
+
+    def testIndexStatus(self):
+        self.conn.create_index("another-index")
+        result = self.conn.status(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertTrue(result.has_key('indices'))
+        self.assertResultContains(result, {'ok': True})
+
+    def testIndexFlush(self):
+        self.conn.create_index("another-index")
+        result = self.conn.flush(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'ok': True})
+
+    def testIndexRefresh(self):
+        self.conn.create_index("another-index")
+        result = self.conn.refresh(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'ok': True})
+
+    def testIndexOptimize(self):
+        self.conn.create_index("another-index")
+        result = self.conn.optimize(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'ok': True})
+
+
+class SearchTestCase(ElasticSearchTestCase):
+    def setUp(self):
+        super(SearchTestCase, self).setUp()
+        self.conn.index({"name":"Joe Tester"}, "test-index", "test-type", 1)
+        self.conn.index({"name":"Bill Baloney"}, "test-index", "test-type", 2)
+        self.conn.refresh(["test-index"])
+
+    def testGetByID(self):
+        result = self.conn.get("test-index", "test-type", 1)
+        self.assertResultContains(result, {'_type': 'test-type', '_id': '1', '_source': {'name': 'Joe Tester'}, '_index': 'test-index'})
+
+    def testGetCountBySearch(self):
+        result = self.conn.count("name:joe")
+        self.assertResultContains(result, {'count': 1})
+
+    def testSearchByField(self):
+        result = self.conn.search("name:joe")
+        self.assertResultContains(result, {'hits': {'hits': [{'_score': 0.19178301, '_type': 'test-type', '_id': '1', '_source': {'name': 'Joe Tester'}, '_index': 'test-index'}], 'total': 1, 'max_score': 0.19178301}})
+
+    def testMLT(self):
+        self.conn.index({"name":"Joe Test"}, "test-index", "test-type", 3)
+        self.conn.refresh(["test-index"])
+        result = self.conn.morelikethis("test-index", "test-type", 1, ['name'], min_term_freq=1, min_doc_freq=1)
+        self.assertResultContains(result, {'hits': {'hits': [{'_score': 0.19178301,'_type': 'test-type', '_id': '3', '_source': {'name': 'Joe Test'}, '_index': 'test-index'}], 'total': 1, 'max_score': 0.19178301}})
+
+if __name__ == "__main__":
+    unittest.main()
